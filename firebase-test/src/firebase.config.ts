8f37ce02b1bff2bcbbccebd1a9ec399a0e1bc6c5
Benjamin Faleye <bofaleye@hotmail.com>
2024-02-13 18:59:06 +0100

    added login screen  design, and  firebase config

---
+++ firebase-test/src/firebase.config.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAFO4jdM1emmuYh0fWv7DTjsmqJQZC8Xt4",
+  authDomain: "fir-test-32f63.firebaseapp.com",
+  projectId: "fir-test-32f63",
+  storageBucket: "fir-test-32f63.appspot.com",
+  messagingSenderId: "1023417520900",
+  appId: "1:1023417520900:web:edf81f49c2d19e1f4cf651",
+  measurementId: "G-EN857GQCZJ"
+};
+
+// Initialize Firebase
+//export const firbase = initializeApp(firebaseConfig);
